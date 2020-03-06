20617a5664af01dba59a2036249a1ca3d78b06f3
scp <45841558+rogan87@users.noreply.github.com>
2020-03-07 00:46:04 +0900

    asdf
    asdf

--- Test/HLT/TimeManagement/Source/TP_SRD_TIM_0051.c
+++ Test/HLT/TimeManagement/Source/TP_SRD_TIM_0051.c
@@ -6,7 +6,7 @@
  *
  * This software contains confidential information of MDS Technology Co.,Ltd.
  * and unauthorized distribution of this software, or any portion of it, are
- * prohibited.
+ * prohdddddibited.
  */
 
 #include <TFX/TFX.h>
